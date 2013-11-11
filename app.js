f7603fd709421078bd9012fabc48c9e9aa430e0d
Yair Yehuda <yair.yehuda@gmail.com>
2013-11-11 16:28:14 +0200

    deleted app.router explicit call

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ app.use(express.favicon('public/images/favicon.ico'));
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
