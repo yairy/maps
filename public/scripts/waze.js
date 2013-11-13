df544124145fa648bff3efde062bd5f2bd1e6095
Yair Yehuda <yair.yehuda@gmail.com>
2013-11-13 22:33:54 +0200

    Used Backbone Fetch

--- public/scripts/waze.js
+++ public/scripts/waze.js
@@ -1,169 +1,178 @@
 (function () {
-	var notifications, map;
+    var map;
 
-if (typeof waze === 'undefined') {
-    waze = {};
-}
-
-Notification = Backbone.Model.extend({ 
-});
-
-NotificationList = Backbone.Collection.extend({ 
-    model : Notification
-});
-
-SingleNotificationView = Backbone.View.extend({
-    tagName:  'div',
-    template: _.template($('#notificationTemplate').html()),
-    events: {
-      'click .title'   : 'expandNotification',
-      'click button'   : 'collapseNotification',
-    },
-    render: function() {
-      this.$el.html(this.template(this.model.toJSON()));
-      return this;
-    },
-    expandNotification : function() {
-      this.$el.find('.more-details').removeClass('hidden');
-    },
-    collapseNotification : function() {
-      this.$el.find('.more-details').addClass('hidden');
-    }
-});
-
-AllNotificationsView = Backbone.View.extend({
-    el: $('div.notification-list'),
-    
-    initialize: function() {
-        this.listenTo(notifications, 'add', this.addOne);
-    },
-    
-    addOne: function(notification) {
-      var view = new SingleNotificationView({ model: notification });
-      this.$el.append(view.render().el);
-    }
-});
-
-MapView = Backbone.View.extend({
-    initialize: function() {
-        var that = this,
-            bounds;
-        
-        this.map = L.map('map').setView([51.505, -0.09], 13);
-        L.tileLayer('http://{s}.tile.cloudmade.com/d1954fa5c5934eecbd0f07c6f7d2d339/997/256/{z}/{x}/{y}.png', {
-                        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-                        maxZoom: 18
-                    }).addTo(this.map);
-        bounds = this.map.getBounds();
-        this.west = bounds.getWest();
-        this.east = bounds.getEast();
-        this.north = bounds.getNorth();
-        this.south = bounds.getSouth();
-        this.center = this.map.getCenter();
-        
-        this.map.on('click', function(e) {
-            var marker = L.marker(e.latlng);
-            marker.addTo(that.map);
-            marker.bindPopup("<p>Hello world!</p>");
-        });
-
-        this.map.on('viewreset', function() {
-            that.refreshBounds();
-            refreshFromServer(that, function(notificationArray) {
-               notifications.add(notificationArray); 
-            });
-        });
-
-        this.map.on('moveend', function() {
-            that.refreshBounds();
-            refreshFromServer(that, function(notificationArray) {
-               notifications.add(notificationArray); 
-            });
-        });
-        
-        this.listenTo(notifications, 'add', this.addOne);
-    },
-    
-    addOne: function(notification) {
-        var lat = notification.get('lat'),
-            lon = notification.get('lon'),
-            marker;
-        
-        if (!lat || !lon) {
-            return;
-        }
-        
-        marker = L.marker([notification.get('lat'), notification.get('lon')]);
-        marker.addTo(this.map);
-    },
-    
-    refreshBounds : function() {
-       	var bounds = this.map.getBounds();
-        this.west = bounds.getWest();
-        this.east = bounds.getEast();
-        this.north = bounds.getNorth();
-        this.south = bounds.getSouth();
-        $("#mapInfo .west").text(this.west.toFixed(4));
-    	$("#mapInfo .south").text(this.south.toFixed(4));		        
-    }
-    
-});
-
-waze.map = (function() {
-
-    function _initNotifications(e) {
-
-        var app;
-
-        notifications = new NotificationList();
-        app = new AllNotificationsView();
-        map = new MapView();
-        refreshFromServer(map, function(notificationArray) {
-           notifications.add(notificationArray); 
-        });
-    }
-    
-    function _initUpdate() {
-        
-        $("#lat").val(map.center.lat);
-        $("#lon").val(map.center.lng);
-        $("#mapActions button").click(function() {
-            debugger;
-            var latlng = new L.LatLng($("#lat").val(), $("#lon").val());
-            map.setView(latlng, map.getZoom());
-        });
+    if (typeof waze === 'undefined') {
+        window.waze = {};
     }
 
-	return {
-		init : function() {
-			_initNotifications();
-            _initUpdate();
-		}
-	}
+    Notification = Backbone.Model.extend();
 
-})();
-    
-function refreshFromServer(map, callback) {
-    var notifications;
-    $.ajax({ 
-        url: ["notifications?west=", map.west, "&east=", map.east, "&north=", map.north, "&south=", map.south].join(""),
-        cache: false
-    }).done(function(data) {
-        notifications = _.map(data, function(el) {
-            return new Notification({ 
-                id : el.id,
-                title : el.title,
-                description : el.description,
-                lon : el.lon,
-                lat : el.lat,
-            });    
-        });
-        if (typeof callback === 'function') {
-            callback(notifications);
+    NotificationList = Backbone.Collection.extend({
+        url : '/notifications'
+    });
+
+    MapModel = Backbone.Model.extend();
+
+    SingleNotificationView = Backbone.View.extend({
+        tagName:  'div',
+        template: _.template($('#notificationTemplate').html()),
+        events: {
+            'click .title span'   : 'expandNotification',
+            'click button'   : 'collapseNotification',
+        },
+        render: function() {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        },
+        expandNotification : function() {
+            this.$('.more-details').removeClass('hidden');
+        },
+        collapseNotification : function() {
+            this.$('.more-details').addClass('hidden');
         }
     });
-}
 
-$(waze.map.init);
+    AllNotificationsView = Backbone.View.extend({
+        el: $('div.notification-list'),
+
+        initialize: function() {
+            this.listenTo(this.collection, 'add', this.addOne);
+            this.listenTo(this.collection, 'reset', this.addAll);
+        },
+
+        addOne: function(notification) {
+            var view = new SingleNotificationView({ model: notification });
+            this.$el.append(view.render().el);
+        },
+        
+        addAll: function() {
+            this.$el.empty();
+            this.collection.each(this.addOne, this);
+        }
+    });
+
+    MapView = Backbone.View.extend({
+        initialize: function() {
+            var that = this,
+                bounds;
+
+            this.map = L.map('map').setView([51.505, -0.09], 13);
+            L.tileLayer('http://{s}.tile.cloudmade.com/d1954fa5c5934eecbd0f07c6f7d2d339/997/256/{z}/{x}/{y}.png', {
+                attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+                maxZoom: 18
+            }).addTo(this.map);
+            this.refreshBounds();
+
+            this.map.on('click', function(e) {
+                var marker = L.marker(e.latlng);
+                marker.addTo(that.map);
+                marker.bindPopup($('#newNotificationTemplate').html());
+            }); 
+
+            this.map.on('viewreset moveend', function() {
+                that.refreshBounds();
+            });
+
+            this.listenTo(this.collection, 'add', this.addOne);
+            this.listenTo(this.collection, 'reset', this.addAll);
+            this.listenTo(this.model, 'change:center', this.panMap)
+        },
+
+        addOne: function(notification) {
+            var lat = notification.get('lat'),
+                lon = notification.get('lon'),
+                marker;
+
+            if (!lat || !lon) {
+                return;
+            }
+
+            marker = L.marker([notification.get('lat'), notification.get('lon')]);
+            marker.addTo(this.map);
+            marker.bindPopup($('#newNotificationTemplate').html());
+        },
+        
+        addAll: function() {
+          this.collection.each(this.addOne, this);  
+        },
+
+        panMap: function() {
+            this.map.panTo(this.model.get('center'));
+        },
+
+        refreshBounds : function() {
+            var bounds = this.map.getBounds();
+            this.model.set({'west' : bounds.getWest(),
+                'east' : bounds.getEast(),
+                'north': bounds.getNorth(),
+                'south': bounds.getSouth()
+            });
+        }
+
+    });
+
+    DebugView = Backbone.View.extend({
+        el: $('#mapInfo'),
+
+        initialize: function() {
+            this.listenTo(this.model, 'change', this.update);
+        },
+
+        update : function(model) {
+            _.each(['west', 'east', 'north', 'south'], function(el) {
+                this.$("." + el).text(model.get(el).toFixed(2));
+            });
+        },
+
+        events: {
+            'click button'   : 'centerButtonClicked'
+        },
+
+        centerButtonClicked: function() {
+            this.model.set('center', [this.$("#lat").val(), this.$("#lon").val()]);
+        }
+
+    });
+
+    waze.map = (function() {
+
+        function _init() {
+
+            var app,
+                mapModel,
+                mapView,
+                debugView,
+                notifications;
+
+            notifications = new NotificationList();
+            mapModel = new MapModel();
+            mapModel.on("change", function() {
+                notifications.fetch({reset: true, url: ["notifications?west=", mapModel.get('west'), "&east=", mapModel.get('east'), "&north=", mapModel.get('north'), "&south=", mapModel.get('south')].join("")});
+            });
+
+            app = new AllNotificationsView({ collection : notifications });
+            mapView = new MapView({ collection : notifications, model : mapModel });
+            debugView = new DebugView({ model : mapModel });
+            
+            _.each(['west', 'east', 'north', 'south'], function(el) {
+                $("#mapInfo ." + el).text(mapModel.get(el).toFixed(2));
+            });
+
+        }
+
+        function _initUpdate() {
+            $("#lat").val(map.center.lat);
+            $("#lon").val(map.center.lng);
+        }
+
+        return {
+            init : function() {
+                _init();
+            }
+        }
+
+    })();
+
+    $(waze.map.init);
 
 })();
