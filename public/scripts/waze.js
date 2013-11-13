8276ccf2a08315d16212976406adba82111e167e
Yair Yehuda <yair.yehuda@gmail.com>
2013-11-13 10:30:30 +0200

    Refresh map from server when moved + Improved model

--- public/scripts/waze.js
+++ public/scripts/waze.js
@@ -5,14 +5,14 @@ if (typeof waze === 'undefined') {
     waze = {};
 }
 
-waze.notification = Backbone.Model.extend({ 
+Notification = Backbone.Model.extend({ 
 });
 
-waze.notificationList = Backbone.Collection.extend({ 
-    model : waze.notification
+NotificationList = Backbone.Collection.extend({ 
+    model : Notification
 });
 
-waze.singleNotificationView = Backbone.View.extend({
+SingleNotificationView = Backbone.View.extend({
     tagName:  'div',
     template: _.template($('#notificationTemplate').html()),
     events: {
@@ -21,7 +21,6 @@ waze.singleNotificationView = Backbone.View.extend({
     },
     render: function() {
       this.$el.html(this.template(this.model.toJSON()));
-      this.model.get('marker').addTo(map);
       return this;
     },
     expandNotification : function() {
@@ -32,85 +31,92 @@ waze.singleNotificationView = Backbone.View.extend({
     }
 });
 
-waze.allNotificationsView = Backbone.View.extend({
+AllNotificationsView = Backbone.View.extend({
     el: $('div.notification-list'),
+    
     initialize: function() {
         this.listenTo(notifications, 'add', this.addOne);
     },
+    
     addOne: function(notification) {
-      var view = new waze.singleNotificationView({ model: notification });
+      var view = new SingleNotificationView({ model: notification });
       this.$el.append(view.render().el);
+    }
+});
+
+MapView = Backbone.View.extend({
+    initialize: function() {
+        var that = this,
+            bounds;
+        
+        this.map = L.map('map').setView([51.505, -0.09], 13);
+        L.tileLayer('http://{s}.tile.cloudmade.com/d1954fa5c5934eecbd0f07c6f7d2d339/997/256/{z}/{x}/{y}.png', {
+                        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+                        maxZoom: 18
+                    }).addTo(this.map);
+        bounds = this.map.getBounds();
+        this.west = bounds.getWest();
+        this.east = bounds.getEast();
+        this.north = bounds.getNorth();
+        this.south = bounds.getSouth();
+        this.center = this.map.getCenter();
+        
+        this.map.on('click', function(e) {
+            var marker = L.marker(e.latlng);
+            marker.addTo(that.map);
+            marker.bindPopup("<p>Hello world!</p>");
+        });
+
+        this.map.on('viewreset', function() {
+            refreshFromServer(that.map, function(notificationArray) {
+               notifications.add(notificationArray); 
+            });
+        });
+
+        this.map.on('move', function() {
+            refreshFromServer(that.map, function(notificationArray) {
+               notifications.add(notificationArray); 
+            });
+        });
+        
+        this.listenTo(notifications, 'add', this.addOne);
     },
-    addAll: function() {
-      Todos.each(this.addOne, this);
+    
+    addOne: function(notification) {
+      var marker = L.marker([notification.get('lat'), notification.get('lon')]);
+      marker.addTo(this.map);
+    },
+    
+    refreshBounds : function() {
+       	var bounds = this.map.getBounds();
+        this.west = bounds.getWest();
+        this.east = bounds.getEast();
+        this.north = bounds.getNorth();
+        this.south = bounds.getSouth();
+        $("#mapInfo .west").text(this.west.toFixed(4));
+    	$("#mapInfo .south").text(this.south.toFixed(4));		        
     }
     
 });
 
 waze.map = (function() {
 
-function _initMap() {
-    map = L.map('map').setView([51.505, -0.09], 13);
-
-    L.tileLayer('http://{s}.tile.cloudmade.com/d1954fa5c5934eecbd0f07c6f7d2d339/997/256/{z}/{x}/{y}.png', {
-                        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-                        maxZoom: 18
-                    }).addTo(map);
-
-        map.on('click', function(e) {
-            var marker = L.marker(e.latlng);
-            marker.addTo(map);
-            marker.bindPopup("<p>Hello world!</p>");
-        });
-        
-        _refreshBounds();
-
-        map.on('viewreset', function(e) {
-        	_refreshBounds();
-        });
-
-        map.on('move', function(e) {
-        	_refreshBounds();
-        });
-	};
-
-	function _refreshBounds() {
-       	var bounds = map.getBounds();
-        $("#mapInfo .west").text(bounds.getWest().toFixed(4));
-    	$("#mapInfo .south").text(bounds.getSouth().toFixed(4));		
-    }
-
     function _initNotifications(e) {
 
-        var bounds = map.getBounds(),
-            app;
+        var app;
 
-        notifications = new waze.notificationList;
-        app = new waze.allNotificationsView();
-        $.ajax({ 
-                url: ["notifications?west=", bounds.getWest(), "&east=", bounds.getEast(), 
-                    "&north=", bounds.getNorth(), "&south=", bounds.getSouth()].join(""),
-                cache: false
-            }).done(function(data) {
-                var notificationsArray = _.map(data, function(el) {
-                    return new waze.notification({ 
-                        id : el.id,
-                        title : el.title,
-                        description : el.description,
-                        lon : el.lon,
-                        lat : el.lat,
-                        marker : L.marker([el.lat, el.lon]  )
-                    });    
-                });
-                notifications.add(notificationsArray);
-            });
+        notifications = new NotificationList();
+        app = new AllNotificationsView();
+        map = new MapView();
+        refreshFromServer(map, function(notificationArray) {
+           notifications.add(notificationArray); 
+        });
     }
     
     function _initUpdate() {
     
-        var latlng = map.getCenter();
-        $("#lat").val(latlng.lat);
-        $("#lon").val(latlng.lng);
+        $("#lat").val(map.center.lat);
+        $("#lon").val(map.center.lng);
         $("#mapInfo button").click(function() {
             var latlng = new L.LatLng($("#lat").val(), $("#lon").val());
             
@@ -121,13 +127,33 @@ function _initMap() {
 
 	return {
 		init : function() {
-			_initMap();
 			_initNotifications();
             _initUpdate();
 		}
 	}
 
 })();
+    
+function refreshFromServer(map, callback) {
+    var notifications;
+    $.ajax({ 
+        url: ["notifications?west=", map.west, "&east=", map.east, "&north=", map.north, "&south=", map.south].join(""),
+        cache: false
+    }).done(function(data) {
+        notifications = _.map(data, function(el) {
+            return new Notification({ 
+                id : el.id,
+                title : el.title,
+                description : el.description,
+                lon : el.lon,
+                lat : el.lat,
+            });    
+        });
+        if (typeof callback === 'function') {
+            callback(notifications);
+        }
+    });
+}
 
 $(waze.map.init);
 
